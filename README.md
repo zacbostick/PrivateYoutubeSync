5cd743fc676d5323693af49038bda19db8ae272e
Zac <zac.bostick@gmail.com>
2023-07-06 12:28:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# PrivateYouTubeSync
+
+PrivateYouTubeSync is a tool designed to bypass the YouTube API restrictions on private and unlisted videos. It fetches data from your organization's own YouTube channel to display the contents in an app, even when the videos are private/unlisted.
+
+## Features
+- Fetches data from your organization's YouTube channel, including private and unlisted videos.
+- Organizes and saves playlist data (including titles and video information) in a local JSON file.
+- Converts the saved data to a CSV file for easy parsing and usage.
+- Downloads thumbnail images from each video in the playlists and saves them in a local directory.
+
+## Installation
+
+Before you begin, make sure you have Node.js installed on your machine.
+
+1. Clone this repository to your local machine:
+    ```bash
+    git clone https://github.com/zac-bostick/PrivateYouTubeSync.git
+    ```
+2. Navigate to the project directory and install the dependencies:
+    ```bash
+    cd PrivateYouTubeSync
+    npm install
+    ```
+3. Start the application by running: 
+    ```bash
+    npm start
+    ```
+    The server will start and listen on port 3000.
+
+### OAuth2 Flow
+- Navigate to http://localhost:3000/ in your web browser.
+- You will be redirected to the Google OAuth2 authorization page.
+- Sign in with the Google account associated with the YouTube channel you want to fetch data from.
+- After successful authentication, the application will fetch all playlists from your YouTube channel, including private and unlisted videos.
+- The fetched data is then saved in playlists_with_titles_and_videos.json, converted to CSV in playlists_with_videos.csv, and all video thumbnails are downloaded to the thumbnails directory.
+
+### Important Notes
+
+- The server runs on port 3000.
+- OAuth2 is used for YouTube API authentication. Make sure you have a valid `credentials.json` file in your project root directory. I have included a mycredentials.json file that is the structure your credentials.json file should follow. You can get these credentials from your google cloud console.
+- The application starts by redirecting to `/authorize` where you authenticate your Google account and grant access to the YouTube Data API.
+- On successful authorization, the application receives an OAuth2 code which it exchanges for access and refresh tokens.
+- The application then fetches all your playlists and their respective videos using the YouTube Data API.
+- The data fetched includes video titles, descriptions, IDs, and high resolution thumbnails, which are saved in a JSON file and also converted to a CSV file.
+- Thumbnails are downloaded and saved in a separate folder.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project is licensed under the terms of the [MIT license](LICENSE.txt).
---
+++ server.js
@@ -0,0 +1,154 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+const https = require('https');
+const path = require('path');
+const credentials = require('./credentials.json');
+const { Parser } = require('json2csv');
+
+const { client_id, client_secret, redirect_uris } = credentials.installed;
+const { google } = require('googleapis');
+const oauth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+const scopes = ['https://www.googleapis.com/auth/youtube.readonly'];
+const authUrl = oauth2Client.generateAuthUrl({ access_type: 'offline', scope: scopes });
+
+async function convertJsonToCsv() {
+  const playlists = require('./playlists_with_titles_and_videos.json');
+
+  const flattenedData = playlists.reduce((arr, playlist) => {
+    playlist.videos.forEach(video => {
+      const thumbnailUrl = video.snippet.thumbnails?.maxres?.url || video.snippet.thumbnails?.high?.url || video.snippet.thumbnails?.default?.url;
+
+      arr.push({
+        playlistId: playlist.id,
+        playlistTitle: playlist.title,
+        videoId: video.contentDetails.videoId,
+        videoTitle: video.snippet.title,
+        videoDescription: video.snippet.description,
+        videoThumbnailUrl: thumbnailUrl,
+        videoPublishedAt: video.snippet.publishedAt
+      });
+    });
+    return arr;
+  }, []);
+
+  const parser = new Parser();
+  const csv = parser.parse(flattenedData);
+
+  fs.writeFileSync('playlists_with_videos.csv', csv);
+  console.log('CSV conversion complete');
+}
+
+function downloadImage(url, dest, cb) {
+  const file = fs.createWriteStream(dest);
+  https.get(url, (response) => {
+    response.pipe(file);
+    file.on('finish', () => {
+      file.close(cb);
+    });
+  });
+}
+
+app.get('/', (req, res) => {
+  res.redirect('/authorize');
+});
+
+app.get('/authorize', (req, res) => {
+  res.redirect(authUrl);
+});
+
+app.get('/api/playlists', (req, res) => {
+  const playlists = require('./playlists.json');
+  res.json(playlists);
+});
+
+app.get('/oauth2callback', (req, res) => {
+  const code = req.query.code;
+  oauth2Client.getToken(code, async (err, tokens) => {
+    if (err) {
+      console.error('Error retrieving access token:', err);
+      return;
+    }
+
+    oauth2Client.setCredentials(tokens);
+    console.log('Successfully retrieved tokens');
+
+    const youtube = google.youtube({
+      version: 'v3',
+      auth: oauth2Client,
+    });
+
+    async function fetchAllPlaylists(pageToken) {
+      const response = await youtube.playlists.list({
+        part: 'snippet,contentDetails',
+        mine: true,
+        maxResults: 50,
+        pageToken: pageToken  
+      });
+
+      let playlists = response.data.items;
+
+      if (response.data.nextPageToken) {
+        const nextPagePlaylists = await fetchAllPlaylists(response.data.nextPageToken);
+        playlists = playlists.concat(nextPagePlaylists);
+      }
+      if (playlists.length > 1) {
+        playlists.pop();
+      }
+      return playlists;
+    }
+
+    const playlists = await fetchAllPlaylists();
+    console.log('Successfully retrieved playlists');
+      
+    for (let playlist of playlists) {
+      const playlistItemsResponse = await youtube.playlistItems.list({
+        playlistId: playlist.id,
+        part: 'snippet,contentDetails',
+        maxResults: 200  
+      });
+      playlist.videos = playlistItemsResponse.data.items;
+    }
+
+    const dataToSave = playlists.map(playlist => {
+      return {
+        id: playlist.id,
+        title: playlist.snippet.title,
+        videos: playlist.videos
+      };
+    });
+
+    fs.writeFileSync('playlists_with_titles_and_videos.json', JSON.stringify(dataToSave, null, 2));
+    console.log('Playlists, their titles and videos saved to playlists_with_titles_and_videos.json');
+
+    await convertJsonToCsv();
+
+    fs.mkdir(path.join(__dirname, 'thumbnails'), { recursive: true }, (err) => {
+      if (err) throw err;
+    });
+
+    dataToSave.forEach((playlist) => {
+      playlist.videos.forEach((video) => {
+        if (video.snippet.thumbnails && video.snippet.thumbnails.maxres) {
+          const imageUrl = video.snippet.thumbnails.maxres.url;
+          const videoId = video.snippet.resourceId.videoId;
+          const imagePath = path.join(__dirname, 'thumbnails', `${videoId}.jpg`);
+
+          downloadImage(imageUrl, imagePath, (err) => {
+            if (err) throw err;
+            console.log(`Downloaded image for video ${videoId}`);
+          });
+        } else {
+          console.log(`No high resolution thumbnail available for video ${video.snippet.resourceId.videoId}`);
+        }
+      });
+    });
+
+    res.send('Playlists, their titles and videos saved. Images are downloading.'); 
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
